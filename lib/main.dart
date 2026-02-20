cd9dea107128d04f7e5b34b09bad0063a619986b
Felannn <felanyoga64@gmail.com>
2026-02-20 16:43:44 +0700

    tambah change notifier provider di main

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:week2/counter_model.dart';
 import 'package:week2/home_page.dart';
+import 'package:provider/provider.dart';
+
 
 void main() {
-  runApp(const MyApp());
+  runApp(ChangeNotifierProvider(
+    create: (context) => CounterModel(),
+    child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
